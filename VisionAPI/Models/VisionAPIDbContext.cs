cceb469a27058c003e02e65e879973a8c695d32b
AndileAndileCattle <andilebshange@gmail.com>
2020-11-05 14:44:53 +0200

    Database 2

--- VisionAPI/Models/VisionAPIDbContext.cs
+++ VisionAPI/Models/VisionAPIDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Security.Claims;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,14 @@ namespace VisionAPI.Models
 {
     public class VisionAPIDbContext : DbContext
     {
-        public VisionAPIDbContext() : base() { }
+        public VisionAPIDbContext() : base("DefaultConnection") 
+        {
+        }
 
         public DbSet<Image> Images { get; set; }
+        public static VisionAPIDbContext Create()
+        {
+            return new VisionAPIDbContext();
+        }
     }
 }
